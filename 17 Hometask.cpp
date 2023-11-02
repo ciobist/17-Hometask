262109bb3eb5eedc0d5efadfcd6a0aefd6ce7674
Nikita MIkhailov <nikita57979@gmail.com>
2023-11-02 16:42:07 +0300

    Добавьте файлы проекта.

---
+++ 17 Hometask.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Vector
+{
+     
+private:
+    
+    double x = 0;
+    double y = 0;
+    double z = 0;
+
+public:
+
+    double LengthOfVector;
+
+    void VectorLength(double _x, double _y, double _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+
+        LengthOfVector = sqrt(x * x + y * y + z * z); 
+
+    }
+
+    void ShowVectorParameters()
+    {
+        cout << " X coordinate = " << x <<  "\n Y coordinate = " << y << "\n Z coordinate = " << z << "\n Vector length = " << LengthOfVector << endl;
+    }
+};
+
+int main()
+{
+    Vector temp;
+    temp.VectorLength(5, 3, 3);
+    temp.ShowVectorParameters();
+
+}
